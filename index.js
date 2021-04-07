5ed2646f67bd4e2142555717411324923988848a
Jamain Arnaud <arnaud.jamain@etu.univ-orleans.fr>
2021-04-07 16:47:04 +0200

    changement

--- index.js
+++ index.js
@@ -9,21 +9,23 @@ bot.on('ready' , function(){
 
 
 bot.on('message' , function(message){
+    message.content.toLowerCase()
     let msg = message.content.split(' ')
     let index = msg.length - 1
     if(msg[index] === 'quoi' ||
         msg[index] === 'quoi.' ||
-        msg[index] === 'QUOI' ||
-        msg[index] === 'POURQUOI'||
+        //msg[index] === 'QUOI' ||
+        //msg[index] === 'POURQUOI'||
         msg[index] === 'pourquoi' ||
-        msg[index] === 'Quoi' ||
-        msg[index] === 'Pourquoi' ||
+        //msg[index] === 'Quoi' ||
+        //msg[index] === 'Pourquoi' ||
         msg[index - 1] === 'quoi'  ||
-        msg[index - 1] === 'pourquoi'  ||
-        msg[index - 1] === 'Pourquoi'  ||
-        msg[index - 1] === 'Quoi'  ||
-        msg[index - 1] === 'QUOI' ||
-        msg[index - 1] === 'POURQUOI') {
+        msg[index - 1] === 'pourquoi'
+        //msg[index - 1] === 'Pourquoi'  ||
+        //msg[index - 1] === 'Quoi'  ||
+        //msg[index - 1] === 'QUOI' ||
+        //msg[index - 1] === 'POURQUOI'
+        ) {
 
         message.channel.send('feur')
     }
