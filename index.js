0fbf1d3fd1c6f96becd10639cd2c90877bb2cc4a
Jamain Arnaud <arnaud.jamain@etu.univ-orleans.fr>
2021-04-08 00:03:04 +0200

    changements

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ bot.on('ready' , function(){
 
 
 bot.on('message' , function(message){
-    let tab = ['quoi' , 'quoI' , 'quOi' , 'quOI' , 'qUoi' , 'qUoI' , 'qUOi' , 'qUOI' , 'Quoi' , 'QuoI' , 'QuOi' , 'QuOI' , 'QUoi' , 'QUoI' , 'QUOi' , 'QUOI' ,
-            'pourquoi' , 'Pourquoi' , 'pk' , 'tfk' , 'Pk' , 'pK' , 'PK' , 'tfK' , 'tFk' , 'tFK' , 'Tfk' , 'TfK' , 'TFk' , 'TFK']
-    let msg = message.content.split(' ')
+    let lower = message.content.toLowerCase()
+    let msg = lower.split(' ')
     let index = msg.length - 1
-    for(let i = 0 ; i < tab.length ; i++) {
-        if(msg[index] === tab[i] || msg[index - 1] === tab[i]) {
-            message.channel.send('feur')
-        }
+
+
+    if(msg[index] === 'quoi' ||
+        msg[index] === 'pourquoi' ||
+        msg[index] === 'pk' ||
+        msg[index] === 'tfk' ||
+        msg[index - 1] === 'quoi' ||
+        msg[index - 1] === 'pourquoi' ||
+        msg[index - 1] === 'pk' ||
+        msg[index - 1] === 'tfk'
+    ) {
+        message.channel.send('feur')
     }
+
 
 })
 
