a50c60f9fbe1f850c224453a262468b666ac6499
Jamain Arnaud <arnaud.jamain@etu.univ-orleans.fr>
2021-04-15 22:57:48 +0200

    changes

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.on('message' , function(message) {
         message.channel.send('nigger')
     }
     if(message.author.tag === 'Iguane45#7177') {
-        let res = Math.random() * gifs.length
+        let res = Math.floor(Math.random() * gifs.length)
         message.channel.send(gifs[res])
     }
 })
